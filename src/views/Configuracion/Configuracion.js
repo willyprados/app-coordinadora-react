e4d2aea6aa3fa29bba12107979f0f1d935a11914
Willy Prado <pradopublicity@gmail.com>
2020-01-20 16:56:45 -0500

    Configuracion

--- src/views/Configuracion/Configuracion.js
+++ src/views/Configuracion/Configuracion.js
@@ -1,6 +1,4 @@
 import React from "react";
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
 import "date-fns";
 import sub from "date-fns/sub";
 import esLocale from "date-fns/locale/es";
@@ -9,21 +7,29 @@ import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker
 } from "@material-ui/pickers";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import Grid from "@material-ui/core/Grid";
-import Search from "@material-ui/icons/Search";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
 // core components
-import CustomInput from "components/CustomInput/CustomInput.js";
-import Button from "components/CustomButtons/Button.js";
-import EditIcon from "@material-ui/icons/Edit";
-import DeleteIcon from "@material-ui/icons/Delete";
-import VisibilityIcon from "@material-ui/icons/Visibility";
-import MUIDataTable from "mui-datatables";
-import styles from "assets/jss/material-dashboard-react/components/headerLinksStyle.js";
+import GridContainer from "components/Grid/GridContainer.js";
+
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
 import { estados, empresas } from "variables/general.js";
 
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
+  }
+}));
+
+// const useStyles = makeStyles(styles);
 
 export default function Configuracion() {
   const classes = useStyles();
@@ -31,102 +37,80 @@ export default function Configuracion() {
     sub(new Date(), { days: 1 })
   );
   const [fechaFin, setFechaFin] = React.useState(new Date());
+  const [estado, setEstado] = React.useState(-1);
+  const [empresa, setEmpresa] = React.useState(-1);
 
   const handleDateChange = (date, caller) => {
     caller === "inicio" && setFechaInicio(date);
     caller === "fin" && setFechaFin(date);
   };
 
-  const actions = (
-    <>
-      <EditIcon />
-      <DeleteIcon />
-      <VisibilityIcon />
-    </>
-  );
-
-  const columns = [
-    "Fecha",
-    "# Pedido",
-    "Empresa Logística",
-    "Estado",
-    "Acciones"
-  ];
-
-  const options = {
-    responsive: "scroll"
+  const handleEstadoChange = e => {
+    const key = e.target.value;
+    setEstado(key);
   };
 
-  /* eslint-disable */
-  const data = [
-    ["04/01/2020", "346523", empresas[0], estados[0], actions],
-    ["04/01/2020", "984624", empresas[1], estados[0], actions],
-    ["04/01/2020", "099184", empresas[1], estados[2], actions],
-    ["04/01/2020", "887146", empresas[3], estados[1], actions],
-    ["04/01/2020", "124485", empresas[2], estados[3], actions],
-    ["04/01/2020", "871452", empresas[4], estados[0], actions],
-    ["04/01/2020", "267354", empresas[0], estados[2], actions],
-    ["04/01/2020", "583774", empresas[1], estados[2], actions],
-    ["04/01/2020", "573366", empresas[4], estados[0], actions],
-    ["04/01/2020", "383632", empresas[0], estados[3], actions],
-    ["04/01/2020", "766436", empresas[3], estados[1], actions],
-    ["04/01/2020", "655134", empresas[1], estados[1], actions],
-    
-  ]
-  /* eslint-enable */
-
+  const handleEmpresaChange = e => {
+    const key = e.target.value;
+    setEmpresa(key);
+  };
+  // const classes = useStyles();
   return (
     <GridContainer>
       <MuiPickersUtilsProvider utils={DateFnsUtils} locale={esLocale}>
-        <Grid container justify="space-around">
-          <KeyboardDatePicker
-            margin="normal"
-            id="fecha-inicio"
-            label="Fecha Inicio"
-            format="dd/MM/yyyy"
-            value={fechaInicio}
-            onChange={date => handleDateChange(date, "inicio")}
-            KeyboardButtonProps={{
-              "aria-label": "change date"
-            }}
-          />
-          <KeyboardDatePicker
-            margin="normal"
-            id="fecha-fin"
-            label="Fecha Fin"
-            format="dd/MM/yyyy"
-            value={fechaFin}
-            onChange={date => handleDateChange(date, "fin")}
-            KeyboardButtonProps={{
-              "aria-label": "change date"
-            }}
-          />
-          <div className={classes.searchWrapper}>
-            <CustomInput
-              formControlProps={{
-                className: classes.margin + " " + classes.search
-              }}
-              inputProps={{
-                placeholder: "Search",
-                inputProps: {
-                  "aria-label": "Search"
-                }
-              }}
-            />
-            <Button color="white" aria-label="edit" justIcon round>
-              <Search />
-            </Button>
-          </div>
-        </Grid>
-      </MuiPickersUtilsProvider>
-      <GridItem xs={12} sm={12} md={12}>
-        <MUIDataTable
-          title={"resultados"}
-          data={data}
-          columns={columns}
-          options={options}
+        <KeyboardDatePicker
+          margin="normal"
+          id="fecha-inicio"
+          label="Fecha Inicio"
+          format="dd/MM/yyyy"
+          value={fechaInicio}
+          onChange={date => handleDateChange(date, "inicio")}
+          KeyboardButtonProps={{
+            "aria-label": "change date"
+          }}
         />
-      </GridItem>
+        <KeyboardDatePicker
+          margin="normal"
+          id="fecha-fin"
+          label="Fecha Fin"
+          format="dd/MM/yyyy"
+          value={fechaFin}
+          onChange={date => handleDateChange(date, "fin")}
+          KeyboardButtonProps={{
+            "aria-label": "change date"
+          }}
+        />
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-simple-select-label">Estado</InputLabel>
+          <Select
+            id="demo-simple-select"
+            value={estado}
+            onChange={handleEstadoChange}
+          >
+            {estados.map((estado, key) => (
+              // eslint-disable-next-line react/jsx-key
+              <MenuItem key={key} value={key}>
+                {estado}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-simple-select-label">Empresa</InputLabel>
+          <Select
+            id="demo-simple-select"
+            value={empresa}
+            onChange={handleEmpresaChange}
+          >
+            {empresas.map((empresa, key) => (
+              // eslint-disable-next-line react/jsx-key
+              <MenuItem key={key} value={key}>
+                {empresa}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </MuiPickersUtilsProvider>
     </GridContainer>
   );
 }
